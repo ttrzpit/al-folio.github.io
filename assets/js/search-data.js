869e35959aa0c255054813446a7aca240a8c7dfd
ttrzpit <48260471+ttrzpit@users.noreply.github.com>
2026-04-12 22:02:02 +0000

    Deploying to gh-pages from @ ttrzpit/al-folio.github.io@07dfd5f28afcec74c1b3fc1c769748d8173be55d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "Research",
-          description: "Research.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/research/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
