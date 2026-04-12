0fe4520bbb7b7d9a0b6e48664fefa912c651043c
ttrzpit <48260471+ttrzpit@users.noreply.github.com>
2026-04-12 22:02:33 +0000

    Deploying to gh-pages from @ ttrzpit/al-folio.github.io@0bfa1d60950b18fcfb6e0baf79093127c9fce2e1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "Projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/Projects/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
