3748a517e671b65137249334dd46e3990db23cbe
ttrzpit <48260471+ttrzpit@users.noreply.github.com>
2026-04-30 19:50:52 +0000

    Deploying to gh-pages from @ ttrzpit/al-folio.github.io@61cbdfafc32f7e674f0ec5a227cd81fd50f1a651 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/Projects/";
           },
+        },{id: "nav-academic-cv",
+          title: "Academic CV",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
