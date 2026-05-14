dda291f0ceca1ec713bffe3ac321616eb52b26c8
ttrzpit <48260471+ttrzpit@users.noreply.github.com>
2026-05-14 23:32:09 +0000

    Deploying to gh-pages from @ ttrzpit/al-folio.github.io@bc3e693fa94c77fd27f84aebaa767e37599d1c5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,8 +416,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-pneumatic-steam-engine",
+          title: 'Pneumatic Steam Engine',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
