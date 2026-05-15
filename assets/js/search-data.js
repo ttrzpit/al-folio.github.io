84a64ef15fc8ed4f57908e4451ff327dadf9b1de
ttrzpit <48260471+ttrzpit@users.noreply.github.com>
2026-05-15 23:35:48 +0000

    Deploying to gh-pages from @ ttrzpit/al-folio.github.io@8c4c0e921ae2cd5a81c6d220698d9e2a704afd36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,9 +426,9 @@ ninja.data = [{
           description: "A wearable designed to keep construction sites safe.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-kinetic-art-installation",
+          title: 'Kinetic Art Installation',
+          description: "Designed and built for artist Terry Berlier.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
