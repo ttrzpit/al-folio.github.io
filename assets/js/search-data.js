e94ec6846f2e2531bb2701ceab3ca8a79d833426
ttrzpit <48260471+ttrzpit@users.noreply.github.com>
2026-05-16 03:15:19 +0000

    Deploying to gh-pages from @ ttrzpit/al-folio.github.io@5c2b346e4ceeb92b9027adbbde19072629b0c2f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,9 +431,9 @@ ninja.data = [{
           description: "Mechatronic system designed produced for artist Terry Berlier.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
