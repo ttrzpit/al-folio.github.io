eafda971238e9a76421e2bb3eda48ddbaee0b4ec
ttrzpit <48260471+ttrzpit@users.noreply.github.com>
2026-05-16 03:27:05 +0000

    Deploying to gh-pages from @ ttrzpit/al-folio.github.io@ba48b82e3aebde9f2c1c3c177b2b89245bf6e6a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,8 +436,8 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
+            },},{id: "projects-kenya-project",
+          title: 'Kenya Project',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
